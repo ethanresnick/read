7bceb2a85e6272c7156f07f26cb61148062c271e
Ethan Resnick <ethan.resnick@gmail.com>
2013-04-25 17:02:56 -0400

    Bug fix
    Only calculate loadedAt when plugin first runs, not each time.

--- read.js
+++ read.js
@@ -1,38 +1,41 @@
-jQuery.fn.read = function(options) {
-    
-    var $this        = $(this).eq(0), //for now, assume we only have one article
-        $window      = $(window),
-        windowHeight = $window.height(),
-        loadedAt     = new Date(),
-        options      = $.extend({
-                        'wordsPerSecond': 25      
-                       }, options),
+(function($) {
+    var loadedAt = new Date();
 
-        article  = {
-            bottom: $this.offset()['top'] + $this.innerHeight(),
-            wordCount: $this.html().split(" ").length
-        },
-            
-        readListener = function() {
-            var scrollTop = $window.scrollTop(),
-                time = (new Date() - loadedAt)/1000,
-                read = (scrollTop + windowHeight >= article.bottom && time >= article.wordCount/options.wordsPerSecond);
+    $.fn.read = function(options) {
 
-            if(read) {
-                $this.trigger('read', [article]);
-                $window.off("scroll.read", readListener);
-		$window.off("resize.read", resizeListener);
-            }
-        },
-		
-        resizeListener = function() {
-            windowHeight = $window.height();
-            article.bottom = $this.offset()['top'] + $this.innerHeight();
-    	};
+        var $this        = $(this).eq(0), //for now, assume we only have one article
+            $window      = $(window),
+            windowHeight = $window.height(),
+            options      = $.extend({
+                            'wordsPerSecond': 25      
+                           }, options),
     
-    $window.on('scroll.read', readListener);
-    $window.on('resize.read', resizeListener);
+            article  = {
+                bottom: $this.offset()['top'] + $this.innerHeight(),
+                wordCount: $this.html().split(" ").length
+            },
+                
+            readListener = function() {
+                var scrollTop = $window.scrollTop(),
+                    time = (new Date() - loadedAt)/1000,
+                    read = (scrollTop + windowHeight >= article.bottom && time >= article.wordCount/options.wordsPerSecond);
     
-     //returning this.each enables chaining
-    return this;
-}
+                if(read) {
+                    $this.trigger('read', [article]);
+                    $window.off("scroll.read", readListener);
+                    $window.off("resize.read", resizeListener);
+                }
+            },
+                    
+            resizeListener = function() {
+                windowHeight = $window.height();
+                article.bottom = $this.offset()['top'] + $this.innerHeight();
+            };
+        
+        $window.on('scroll.read', readListener);
+        $window.on('resize.read', resizeListener);
+        
+         //returning this.each enables chaining
+        return this;
+    }
+})(jQuery);
